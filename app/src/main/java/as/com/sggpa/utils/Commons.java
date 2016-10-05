6038c032a69be19667d879044d30d8b52b7257ad
antonsalim <anton_salim@hotmail.com>
2016-10-05 13:58:42 +0800

    Removed firebase and permissions to fix crash bug

--- app/src/main/java/as/com/sggpa/utils/Commons.java
+++ app/src/main/java/as/com/sggpa/utils/Commons.java
@@ -7,9 +7,4 @@ import android.telephony.TelephonyManager;
  * Created by anton on 2/10/2016.
  */
 public class Commons {
-    public static String getDeviceID(Context context)
-    {
-        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        return telephonyManager.getDeviceId();
-    }
 }
